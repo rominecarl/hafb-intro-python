10306530f45906cbce5b0831483e71d9acb18904
rominecarl <rominecarl+git@gmail.com>
2019-08-05 14:49:41 -0600

    Added dictionaries

--- words.py
+++ words.py
@@ -9,10 +9,16 @@ from urllib.request import urlopen
 file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
 
 count = 0
+data = {}
 with urlopen(file) as story:
-    for line in story:
-        words = line.decode('utf-8').split()
-        for word in words:
+    for line in story:                          # reads the file one line at a time
+        words = line.decode('utf-8').split()    # parse the line into words. Split defaults to spaces
+        for word in words:                      # read through the word dictionary
+            if word in data:                    # test for existance. I.e. does the entry already exist in the dictionary
+                data[word] += 1
+            else:                               # data not found
+                data[word] = 1
             count += 1
 print("Total number of words", count)
+print ("Total data", data)
 
