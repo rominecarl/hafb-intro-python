606a34ed7119cba558daec7076633048a8ba31c5
rominecarl <rominecarl+git@gmail.com>
2019-08-05 13:36:44 -0600

    Added dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,19 @@
+"""
+Learn dictionaries
+Dictionaries maps keys to values.
+
+In some languages are known as associative arrays, hashes, or maps.
+
+Create using {} containing a key-value pair
+Retrieve them by [key_value]
+
+"""
+
+#first item is the key, 2nd is the value
+d = {'alice':'801-123-8988',
+    'pedro':'956-445-78-8966',
+    'john':'651-321-66-4477'}
+print(d, type(d))
+# Access one element
+print(d['pedro'])
+
