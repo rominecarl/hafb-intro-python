4d17baca81dcd83558d6616a78919408795425ad
CCEClass1 <CCEClass1@ad.weber.edu>
2019-08-05 10:13:48 -0600

    Initial commit

---
+++ test.py
@@ -0,0 +1,26 @@
+# print("hello python")
+# Scalar data types: Integers, Floating point
+#None, and Boolean
+
+print("Binary notation", 0b0100)
+print("hex notation:", 0x0100)
+
+#use the int() constructor
+print(int("477"))
+
+#option for base
+print(int("1001", 2))
+
+#floating point: IEEE-754
+#scientific notation
+print("Scientific Notation:",1.61e-35)
+
+print("Float construction float()" ,   float("1.99"))
+#Boolean
+print("Boolean const.bool()", bool(0))
+print("Boolean const. bool(42", bool(42))
+print("Boolean const. bool(0.1)", bool(0.1))
+#Test boolean on arrays <-- empty = false, non empty = true
+print("Boolean const.bool([])", bool([]))
+print("Boolean const.bool([1,2,3])", bool([1,2,3]))
+print("Bool conts. bool("")")
