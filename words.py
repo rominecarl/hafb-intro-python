96a1bafac7334cbeaf71ef91fd66f4b5288166bc
rominecarl <rominecarl+git@gmail.com>
2019-08-05 15:48:55 -0600

    functionized fetch_words

--- words.py
+++ words.py
@@ -2,27 +2,45 @@
 Get a file from the Web
 http://icarus.cs.weber.edu/~hvalle/hafb/words.txt
 
-Task 1: Count number of words in document
 """
 
-from urllib.request import urlopen
-file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
 
-count = 0
-data = {}
-with urlopen(file) as story:
-    for line in story:                          # reads the file one line at a time
-        words = line.decode('utf-8').split()    # parse the line into words. Split defaults to spaces
-        for word in words:                      # read through the word dictionary
-            if word in data:                    # test for existance. I.e. does the entry already exist in the dictionary
-                data[word] += 1
-            else:                               # data not found
-                data[word] = 1
-            count += 1
-print("Total number of words", count)
-print ("Total data", data)
 
-# sort by keys
-for key in sorted(data.keys()):
-    print(key, data[key])
+def fetch_words():
+    """
+    Fetch the words  a sorted word list with word counts from a prespecified URL
+    :return: None
+    :print: Word list
+    """
+
+    from urllib.request import urlopen
+    file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
+    count = 0
+    data = {}
+    with urlopen(file) as story:
+        for line in story:                          # reads the file one line at a time
+            words = line.decode('utf-8').split()    # parse the line into words. Split defaults to spaces
+            for word in words:                      # read through the word dictionary
+                if word in data:                    # test for existance. I.e. does the entry already exist in the dictionary
+                    data[word] += 1
+                else:                               # data not found
+                    data[word] = 1
+                count += 1
+    print("Total number of words", count)
+    print ("Total data", data)
+
+    # sort by keys
+    for key in sorted(data.keys()):
+        print(key, data[key])
+
+def main():
+    """
+    Test function
+    :return: nothing
+    """
+    fetch_words()
+
+if __name__ == "__main__":
+    main()
+    exit(0)
 
