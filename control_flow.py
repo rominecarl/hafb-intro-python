a9ed5e7030069c672ad1cb1ffe88dbcea6fd85cb
rominecarl <rominecarl+git@gmail.com>
2019-08-05 11:06:17 -0600

    Added conditionals

--- control_flow.py
+++ control_flow.py
@@ -8,4 +8,25 @@ if True:
     print("It is true")
 
 print("Done")
-#what
+
+if bool("eggs"):
+    print("Yes please!")
+
+if "eggs":
+    print("Yes, why not")
+
+#Flat is better than nested
+h=42
+if h > 50:
+    print("Greater than 50")
+    if h > 100:
+        print("Greater than 100")
+elif h < 50: #Else If
+    print("Less than 50")
+else:
+    print("Equals 50")
+
+
+
+#####
+print("done")
