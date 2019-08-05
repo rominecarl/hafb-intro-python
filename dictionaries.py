582df3503098bf40a629a13219c31d5819433c79
rominecarl <rominecarl+git@gmail.com>
2019-08-05 14:00:36 -0600

    Added dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -17,3 +17,19 @@ print(d, type(d))
 # Access one element
 print(d['pedro'])
 
+# add members to the dictionary, of names -> grades
+roster = {}             # Empty dictionary
+d['john'] = 7           # if entry already exists
+d['tyler'] = 'bonehead' # if entry does not already exist, gets added
+
+print(d)
+
+names = {}
+total = 0
+while total <3:
+    name = input("Enter a name: ")
+    value = input("Enter a value: ")
+    names[name] = value
+    total = total + 1
+
+print(names)
