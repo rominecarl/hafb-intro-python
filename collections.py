7816a09fa6579f77a05370d8283a8d2f939c0c80
carlromine <rominecarl+git@gmail.com>
2019-08-06 14:46:36 -0600

    initial commit

---
+++ collections.py
@@ -0,0 +1,16 @@
+"""
+Learn about collections: Tuples, Strings, Range, List, Dictionaries, Sets
+"""
+
+
+def main():
+    """
+    Test function
+    :return: 
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
