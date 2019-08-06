a8022b5786d16ab555df9ff12ee3b34782af3321
carlromine <rominecarl+git@gmail.com>
2019-08-06 09:45:34 -0600

    initial commit

--- take_break.py
+++ take_break.py
@@ -3,14 +3,21 @@ Set an alarm to take a break
 """
 
 import webbrowser
+import time
 
 def main():
     """
     Test function
     :return: none
     """
-    video_address = "https://www.youtube.com/watch?v=Mg91VsButWE"
-    webbrowser.open(video_address)
+    counter = 0
+    while counter < 3:
+        video_address = "https://www.youtube.com/watch?v=Mg91VsButWE"
+        # Delay "sleep"
+        time.sleep(60 * 60)
+        print("it is time to take a break. Time is: ", time.ctime())
+        webbrowser.open(video_address)
+        counter += 1
 
 if __name__== '__main__':
     main()
