8273ada2194532f1ac337398bd38533007308e97
carlromine <rominecarl+git@gmail.com>
2019-08-06 12:48:31 -0600

    added replace_content() function

--- reference.py
+++ reference.py
@@ -24,6 +24,19 @@ def replace(g):
     g = [17,48,89]  # <== this overwrites the list that was passed in as a reference. Probably not what you really wanted to do.
     print("g = ", g)
 
+def replace_content(g):
+    """
+    Replace the content of the input list
+    :param g:
+    :return:
+    """
+    g[0] = 88
+    g[1] = 22
+    g[2] = 44
+    print("g = ", g)
+
+
+
 def main():
     """
     test function
@@ -31,10 +44,15 @@ def main():
     """
     m = [9,15,24]
     print("Before: m = ", m)
+
     modify(m)
     print("After: m = ", m)
+
     replace(m)
     print("after replace() m = ", m)
+
+    replace_content(m)
+    print("After replace_content() m =", m)
 
 if __name__ == '__main__':
     main()
