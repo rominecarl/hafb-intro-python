b62da7a848ff12658b041a249678d54a9858fb6c
carlromine <rominecarl+git@gmail.com>
2019-08-06 12:56:53 -0600

    initial commit

---
+++ return_semantics.py
@@ -0,0 +1,16 @@
+"""
+Learn about Python return semantics and how Python functions handle arguments
+"""
+
+
+def main():
+    """
+    Test function
+    :return:
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
