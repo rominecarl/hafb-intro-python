72033395dd4ff31e5e189bc7536bfbdeccd1507b
carlromine <rominecarl+git@gmail.com>
2019-08-06 09:32:46 -0600

    initial commit

---
+++ take_break.py
@@ -0,0 +1,18 @@
+"""
+Set an alarm to take a break
+"""
+
+import webbrowser
+
+def main():
+    """
+    Test function
+    :return: none
+    """
+    video_address = "https://www.youtube.com/watch?v=Mg91VsButWE"
+    webbrowser.open(video_address)
+
+if __name__== '__main__':
+    main()
+    exit(0)
+
