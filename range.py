e76e4abf37a4c2befea90837496d7960ca37bc22
rominecarl <rominecarl+git@gmail.com>
2019-08-07 08:35:18 -0600

    initial commit

---
+++ range.py
@@ -0,0 +1,21 @@
+"""
+Purpose: Learn about range() collections
+"""
+
+
+def main():
+    """
+    Test function
+    :return: 
+    """
+    for i in range(5):
+        print(i)
+
+    # Set the initial value
+    print("Now setting initial value")
+    for i in range(5,10):
+        print(i)
+
+if __name__ == "__main__":
+    main()
+    exit(0)
