51906a6687b66d3ee297514f2cea92271f3b58fd
carlromine <rominecarl+git@gmail.com>
2019-08-08 13:48:42 -0600

    Day 4 PM

--- airtravel.py
+++ airtravel.py
@@ -4,6 +4,65 @@ Day 4 - 12:30
 """
 
 
+class Flight:
+    """
+    A Flight with a particular passenger aircraft
+    """
+
+    # Python doesn't use the term constructor for technical reasons (nothing is actully constructed)
+    # Python doesn't distinguish between public, private, friend, etc.
+    def __init__(self, number):
+        """
+        Initializes flight number
+        :param number: the flight number you want to initialize
+        implementations details begin with '_'
+        :raise: ValueError (For invalid format)
+        """
+
+        # Note, to be valid, a flight number has the format LL### i.e. 2 alpha characters followed by 3 numbers
+        # Validate flight number: 5 chars long, AADDD A->ALPHA, D->Digit
+        # Check for 5 chars long
+        if len(number) != 5:
+            raise ValueError("Invalid flight number length".format(number))
+
+        # Check that 1st two characters are alpha
+        if not number[:2].isalpha():
+            raise ValueError("No airline code {}".format(number))
+
+        # Check that 1st two characters are upper case
+        if not number[:2].isupper():
+            raise ValueError("Invalid Airline code [not uppercase: {}]".format(number))
+
+        if not number[2:].isdigit():
+            raise ValueError("Invalid route code {}".format(number))
+        self._number = number  # single _ is python convention that this variable should be considered private. In reality you still have access
+
+    def number(self):
+        """
+        Flight Number
+        :return: flight number
+        """
+        return self._number[2:]
+
+    def airline(self):
+        return self._number[:2]
+
+
+class Aircraft:
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+        # these variables persist throughout the life of the object
+
+    def registration(self):
+        return self._registration
+
+    def model(self):
+        return self._model
+
+
 def main():
     """
     Test function
--- use_airtravel.py
+++ use_airtravel.py
@@ -2,15 +2,28 @@
 Purpose: Use flight class
 Day 4 - 12:30
 """
-
+from airtravel import Flight, Aircraft
 
 def main():
     """
     Test function
     :return: 
     """
-    pass
 
+    f = Flight("SN066")
+    print(f, type(f))
+    print(f.number())
+    print(f.airline())
+    # Could use: Flight.number(f) <-- no one uses this approach. just mentioned for reference
+
+    # f2 = Flight("ab345")
+    # print(f2.number())
+
+    # f3 = Flight("AB352")
+    # print(f3.number())
+
+    a1 = Aircraft("G-EUP", "Airbus A319", num_rows=22, num_seats_per_row=6,)
+    print(a1.registration(), a1.model())
 
 if __name__ == "__main__":
     main()
