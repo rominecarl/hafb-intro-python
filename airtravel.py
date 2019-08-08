a5a1992e7fb75e117247ec1f2cdefeb97faed76c
carlromine <rominecarl+git@gmail.com>
2019-08-08 12:35:06 -0600

    Day 4 PM - initial commit

---
+++ airtravel.py
@@ -0,0 +1,17 @@
+"""
+Purpose: A Flight Class. Model for aircraft flights
+Day 4 - 12:30
+"""
+
+
+def main():
+    """
+    Test function
+    :return: 
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
