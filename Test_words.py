f99a76ee1c12c69ae0b16806f83b48f663954b0b
rominec <rominecarl+git@gmail.com>
2019-08-31 22:25:51 -0600

    Testing commit functionality

--- Test_words.py
+++ Test_words.py
@@ -1,9 +1,7 @@
 """
-Get a file from the Web
-http://icarus.cs.weber.edu/~hvalle/hafb/words.txt
-
+Testing the code originally written in the words.py file. Working through the example again to see how they work
+13 Aug 19
 """
-
 
 def fetch_words():
     """
@@ -28,29 +26,26 @@ def fetch_words():
     print("Total number of words", count)
     print ("Total data", data)
 
-    # sort by keys
-    for key in sorted(data.keys()):
-        print(key, data[key])
 
-
-def print_items(items):
-    """
-    Print elements of the collection
-    :param items: A collections of objects
-    :return: nothing
-    """
-    for item in items:
-        print(item)
-
+def try_dict():
+    my_dict = {}
+    print(my_dict)
+    my_dict[0] = "bubba"
+    my_dict[2] = "gump"
+    my_dict[5000] = "third entry"
+    my_dict["test"] = "this was a test"
+    print(my_dict)
+    print(my_dict[5000])
+    # Note, this test shows me that the "index" to a dictionary can be anything, not just an integer.
 
 def main():
     """
     Test function
     :return: nothing
     """
-    fetch_words()
+#    fetch_words()
+    try_dict()
 
 if __name__ == "__main__":
     main()
     exit(0)
-
